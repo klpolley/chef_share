9583032c7cf1ddab90d0406ffd30e4c24ce000d9
Kate Polley <kpolley@ithaca.edu>
2020-03-25 14:18:12 -0400

    project setup

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTest {
+
+    @Test
+    void testPlaceHolder() {
+        assertTrue(true);
+    }
+
+}
