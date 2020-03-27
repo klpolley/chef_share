23a8a7712e79b8a256f1260a3d6dc2eb919b7f52
sweeks19 <sweeks@ithaca.edu>
2020-03-26 20:40:09 -0400

    food class tests and constructor

--- src/main/java/Food.java
+++ src/main/java/Food.java
@@ -1,2 +1,26 @@
 public class Food {
+    private String name;
+    private int calories;
+
+    public Food(String nameIn, int caloriesIn) throws IllegalArgumentException{
+        if (nameIn == "" || nameIn==" "){
+            throw new IllegalArgumentException("food name cannot be empty");
+        }
+        else if (caloriesIn < 1){
+            throw new IllegalArgumentException("calories must be a positive value greater than 0");
+        }
+        else{
+            name = nameIn;
+            calories = caloriesIn;
+        }
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getCalories(){
+        return calories;
+    }
 }
--- src/test/java/FoodTest.java
+++ src/test/java/FoodTest.java
@@ -1,2 +1,36 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FoodTest {
+
+    @Test
+    void constructorTest(){
+
+        //food with positive calorie amount
+        Food pickle = new Food("pickle", 550);
+        Food cookie = new Food("cookie", 800);
+        Food banana = new Food("banana", 250);
+
+        assertEquals("pickle", pickle.getName());
+        assertEquals(550, pickle.getCalories());
+        assertEquals("cookie", cookie.getName());
+        assertEquals(800, cookie.getCalories());
+        assertEquals("banana", banana.getName());
+        assertEquals(250, banana.getCalories());
+
+        //negative calorie amount
+        assertThrows(IllegalArgumentException.class, ()-> new Food("squash", -400));
+        assertThrows(IllegalArgumentException.class, ()-> new Food("brownie", -900));
+
+        //zero calorie amount
+        assertThrows(IllegalArgumentException.class, ()-> new Food("squash", 0));
+
+        //empty string name
+        //no space
+        assertThrows(IllegalArgumentException.class, ()-> new Food("", 700));
+        //one space
+        assertThrows(IllegalArgumentException.class, ()-> new Food(" ", 700));
+    }
+
+
 }
