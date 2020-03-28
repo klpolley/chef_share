d7c723345ca4f08c5433099195fd66b611971481
Kate Polley <kpolley@ithaca.edu>
2020-03-27 22:19:10 -0400

    starting account container setup

---
+++ src/main/java/AccountContainer.java
@@ -0,0 +1,54 @@
+public class AccountContainer {
+
+    private Map<String, Account> accounts = new Map<>();
+
+    private Account currentAccount = null;
+
+    public void createAccount(String username, String password, String bio) {
+
+    }
+
+    public void removeAccount(String username) {
+
+    }
+
+    public void updateUsername(String username, String newUsername) {
+
+    }
+
+    public void updatePassword(String username, String newPassword) {
+
+    }
+
+    public void updateBiography(String username, String newBio) {
+
+    }
+
+    public String getUserBio(String username) {
+
+    }
+
+    public void login(String username, String password) {
+
+    }
+
+    public void logout(String username) {
+
+    }
+
+    public boolean confirmCredentials(String username, String password) {
+
+    }
+
+    public boolean accountExists(String username) {
+
+    }
+
+    public Account getCurrentAccount() {
+
+    }
+
+
+
+
+}
