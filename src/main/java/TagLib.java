c39feef3fd1c9b110d1804e96c1c45e0b2faad8c
Stephen <stevoschool@gmail.com>
2020-04-10 15:06:51 -0400

    tests added and failing

---
+++ src/main/java/TagLib.java
@@ -0,0 +1,30 @@
+
+
+import java.util.ArrayList;
+
+public class TagLib {
+    private String[] tags;
+    private int numTags;
+
+    public TagLib(){}
+    public String printTags(){
+        return "";
+    }
+    public void addTag(String tagIn) throws IllegalArgumentException{}
+    public boolean isTag(String tagIn) throws IllegalArgumentException{
+        return false;
+    }
+    public String[] search(String subTag) throws IllegalArgumentException{
+        return null;
+    }
+    public int numTags(){
+        return -1000;
+    }
+    private void doubleLength(){}
+
+    public static boolean validTag(String tag){
+        return false;
+    }
+
+
+}
