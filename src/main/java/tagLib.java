663c96f042f51cff0dd95bb89a2f63c6516001cc
Stephen <stevoschool@gmail.com>
2020-04-09 20:14:09 -0400

    forgot something quick commit

---
+++ src/main/java/tagLib.java
@@ -0,0 +1,23 @@
+
+
+import java.util.ArrayList;
+
+public class tagLib {
+    private String[] tags;
+    private int numTags;
+
+    public tagLib(){}
+    public void addTag(String tagIn) throws IllegalArgumentException{}
+    public boolean hasTag(String tagIn) throws IllegalArgumentException{
+        return false;
+    }
+    public ArrayList<String> search(String subTag) throws IllegalArgumentException{
+        return null;
+    }
+    public int numTags(){
+        return -1000;
+    }
+    private void doubleLength(){}
+
+
+}
